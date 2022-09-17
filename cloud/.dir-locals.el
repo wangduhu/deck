57da42be273e7bfb84c4ccfae8619e1944178a02
wally <buaaben@163.com>
2022-09-17 19:25:18 +0800

    feat: sync config from cloud

---
+++ cloud/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil . (
+         (ssh-deploy-root-local . "/Users/wally/Project/deck/cloud/")
+         (ssh-deploy-root-remote . "/ssh:root@47.93.215.136:/home/wally/deck/")
+         (ssh-deploy-on-explicit-save . 1)
+         (ssh-deploy-force-on-explicit-save . 1)
+         )))
