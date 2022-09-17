b97a70dd2bf2bac681f67d4c790ac7d942269e6f
wally <buaaben@163.com>
2022-09-17 14:37:00 +0800

    ci: 调整目录结构

---
+++ jupiter/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil . (
+         (ssh-deploy-root-local . "/Users/wally/Project/deck/jupiter/")
+         (ssh-deploy-root-remote . "/ssh:wally@192.168.1.2:/home/wally/Project/deck/")
+         (ssh-deploy-on-explicit-save . 1)
+         (ssh-deploy-force-on-explicit-save . 1)
+         )))
